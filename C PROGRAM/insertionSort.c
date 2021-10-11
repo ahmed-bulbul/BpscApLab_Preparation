171045f8d13ba733d956ae02c0224e53eeb57413
Bulbul Ahmed <70557643+bulbul-deploy@users.noreply.github.com>
2021-10-11 09:28:53 +0600

    Linked List

---
+++ C PROGRAM/insertionSort.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+
+
+  int arr[]={5,40,2,1,5,9};
+  int size =sizeof(arr)/sizeof(arr[0]);
+
+  for(int i=1;i<size;i++){
+
+    int value = arr[i];
+    int hole = i;
+
+    while(hole>0 && value  < arr[hole-1]){
+        arr[hole] = arr[hole-1];
+        hole--;
+    }
+    arr[hole]=value;
+  }
+
+
+  //printing the array
+  for(int i=0;i<size;i++){
+    printf("%d  ",arr[i]);
+  }
+
+}
